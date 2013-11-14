23fc89bef98b26efbc58b6fc1d6799413c42d8a7
Nicholas Rakotomihamina <nico@nrako.com>
2013-11-14 18:19:57 +0100

    add commented settings to evaluate

--- modules/people/manifests/nrako/osx.pp
+++ modules/people/manifests/nrako/osx.pp
@@ -1,17 +1,25 @@
 class people::nrako::osx {
 
-  # include osx::dock::2d
-  # include osx::dock::dim_hidden_apps
 
-  # include osx::finder::show_all_on_desktop
-  # include osx::finder::unhide_library
+  osx::recovery_message { 'If this Mac is found, please call +41 79 176 32 52 or email me at nico@nrako.com': }
 
-  # class { 'osx::global::key_repeat_delay': delay => 10 }
-  # class { 'osx::global::key_repeat_rate': rate => 2 }
+  include osx::finder::unhide_library
 
-  # include osx::global::disable_autocorrect
-  # include osx::global::expand_print_dialog
-  # include osx::global::expand_save_dialog
+  class { 'osx::global::key_repeat_delay': delay => 0 }
+  class { 'osx::global::key_repeat_rate': rate => 2 }
+
+  include osx::global::disable_autocorrect
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
+
+  include osx::dock::autohide
+  class { 'osx::dock::icon_size': 
+    size => 42
+  }
+
+  include osx::finder::empty_trash_securely
+
+  # TODOS
 
   # boxen::osx_defaults {
   #   'Disable the dashboard':
@@ -30,4 +38,46 @@ class people::nrako::osx {
   #     type   => 'bool',
   #     user   => $::boxen_user;
   # }
+
+  # Trackpad: enable tap to click for this user and for the login screen
+  # defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+  # defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+  # defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+  # defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+  # defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerVertSwipeGesture -int 2
+  # defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerDoubleTapGesture -int 1
+  # defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerTapGesture -int 2
+  # defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 3
+
+  # Don’t show Dashboard as a Space
+  # defaults write com.apple.dock dashboard-in-overlay -bool true
+
+  # Don’t automatically rearrange Spaces based on most recent use
+  # defaults write com.apple.dock mru-spaces -bool false
+
+  # Make Dock icons of hidden applications translucent
+  # defaults write com.apple.dock showhidden -bool true
+
+  # Don’t display the annoying prompt when quitting iTerm
+  # defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+  # Enable Safari’s debug menu
+  # defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+  # Enable the Develop menu and the Web Inspector in Safari
+  # defaults write com.apple.Safari IncludeDevelopMenu -bool true
+  # defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+  # defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+  # Add a context menu item for showing the Web Inspector in web views
+  # defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+
+  ###############################################################################
+  # Time Machine                                                                #
+  ###############################################################################
+
+  # Prevent Time Machine from prompting to use new hard drives as backup volume
+  # defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 }
